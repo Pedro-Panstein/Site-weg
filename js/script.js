0b039377bb84a23e0d57d56828221a78987e4121
Pedro Henrique Panstein <phfarof@gmail.com>
2024-05-16 15:55:33 -0300

    atualizado

--- js/script.js
+++ js/script.js
@@ -5,3 +5,14 @@ setor.addEventListener("click", function(){
     window.location.href="setor.html"
 
 });
+
+function menuShow() {
+    let menuMobile = document.querySelector('.mobile-menu');
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = "image/menu_white_36dp.svg";
+    } else {
+        menuMobile.classList.add('open');
+        document.querySelector('.icon').src = "image/close_white_36dp.svg";
+    }
+}
