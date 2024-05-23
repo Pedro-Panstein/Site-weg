efc5f0219f89a86cde9f8361a571e9b462893f29
Pedro Henrique Panstein <phfarof@gmail.com>
2024-05-23 14:36:21 -0300

    comit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,60 @@
+
+const metas = document.getElementById("metas");
+
+metas.addEventListener("click", function(){
+    
+    window.location.href="metas.html"
+    
+});
+
+const datas = document.getElementById("datas");
+
+datas.addEventListener("click", function(){
+    
+    window.location.href="datas.html"
+    
+});
+
+const novidades = document.getElementById("novidades");
+
+novidades.addEventListener("click", function(){
+    
+    window.location.href="novidade.html"
+    
+});
+
+const noticias = document.getElementById("noticias");
+
+noticias.addEventListener("click", function(){
+    
+    window.location.href="noticia.html"
+    
+});
+
+const avisos = document.getElementById("avisos");
+
+avisos.addEventListener("click", function(){
+
+    window.location.href="avisos.html"
+    
+});
+
+const eventos = document.getElementById("eventos");
+
+eventos.addEventListener("click", function(){
+    
+    window.location.href="eventos.html"
+    
+});
+
+const aniversario = document.getElementById("aniversario");
+
+aniversario.addEventListener("click", function(){
+    
+    window.location.href="aniversariantes.html"
+    
+});
+
 const setor = document.getElementById("setor");
 
 setor.addEventListener("click", function(){
@@ -6,22 +63,53 @@ setor.addEventListener("click", function(){
 
 });
 
-const metas = document.getElementById("metas");
+const cardapio = document.getElementById("cardapio");
 
-metas.addEventListener("click", function(){
-
-    window.location.href="metas.html"
-
+cardapio.addEventListener("click", function(){
+    
+    window.location.href="cardapio.html"
+    
 });
 
-const datas = document.getElementById("datas");
+const curiosidade = document.getElementById("curiosidade");
 
-datas.addEventListener("click", function(){
-
-    window.location.href="datas.html"
-
+curiosidade.addEventListener("click", function(){
+    
+    window.location.href="curiosidade.html"
+    
 });
 
+const contrato = document.getElementById("contrato");
+
+contrato.addEventListener("click", function(){
+    
+    window.location.href="contrato.html"
+    
+});
+
+const saude = document.getElementById("saude");
+
+saude.addEventListener("click", function(){
+    
+    window.location.href="saude.html"
+    
+});
+
+const feedback = document.getElementById("feedback");
+
+feedback.addEventListener("click", function(){
+    
+    window.location.href="feedback.html"
+    
+});
+
+const acidente = document.getElementById("acidente");
+
+acidente.addEventListener("click", function(){
+    
+    window.location.href="acidente.html"
+    
+});
 
 function menuShow() {
     let menuMobile = document.querySelector('.mobile-menu');
